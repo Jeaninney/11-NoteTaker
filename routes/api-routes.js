e359d495aa6aa7db2d62580e3db340216f6653cd
Jeaninne <jeaninnel@hotmail.com>
2020-06-07 21:52:48 -0400

    post route now save the notes and updates the db.json file

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,19 +1,24 @@
 var dbjson = require("../db/db.json");
 const fs = require("fs");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-  app.get("/api/notes", function(req, res) {
+  app.get("/api/notes", function (req, res) {
     res.json(dbjson);
   });
 
-  app.post("/api/notes", function(req, res) {
-      var notes = req.body;
-      console.log(notes);
-      return notes;
+  app.post("/api/notes", function (req, res) {
+    var notes = req.body;
+    console.log(notes);
+    dbjson.push(notes);
+
+    fs.writeFile("./db/db.json", JSON.stringify(dbjson), function (err) {
+      if (err) throw (err);
+    })
+    res.json(dbjson);
   });
 
-  app.delete("/api/notes:id", function(req, res) {
+  app.delete("/api/notes:id", function (req, res) {
 
   });
 };
