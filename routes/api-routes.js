401b4be69f3ff9d4e1401d90dcda06d662e703b7
Jeaninne <jeaninnel@hotmail.com>
2020-06-07 22:23:20 -0400

    added delete route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,6 @@
-var dbjson = require("../db/db.json");
+const dbjson = require("../db/db.json");
 const fs = require("fs");
-let id = dbjson.length +1;
+let id = dbjson.length + 1;
 
 module.exports = function (app) {
 
@@ -20,7 +20,17 @@ module.exports = function (app) {
     res.json(dbjson);
   });
 
-  app.delete("/api/notes:id", function (req, res) {
+  app.delete("/api/notes/:id", function (req, res) {
+    const id = parseInt(req.params.id);
+    for (var i = 0; i < dbjson.length; i++) {
+      if (dbjson[i].id === id) {
+        dbjson.splice(i, 1);
+      }
+    }
+    fs.writeFile("./db/db.json", JSON.stringify(dbjson), function (err){
+      if (err) throw (err);
+    })
 
+    res.json(dbjson);
   });
 };
